b4b738ca074cf317b011635c5683ab9eccb94b2b
bendo <zdeno.osina@gmail.com>
2016-02-01 23:47:31 +0100

    formating

--- src/components/Hello.js
+++ src/components/Hello.js
@@ -2,30 +2,30 @@ import React from 'react';
 import World from './World';
 
 export default class Hello extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {model: props.store.getState()};
-        this.onChange = this.onChange.bind(this);
-        this.componentDidMount = this.componentDidMount.bind(this);
-        this.componentWillUnmount = this.componentWillUnmount.bind(this);
-    }
+  constructor(props) {
+    super(props);
+    this.state = {model: props.store.getState()};
+    this.onChange = this.onChange.bind(this);
+    this.componentDidMount = this.componentDidMount.bind(this);
+    this.componentWillUnmount = this.componentWillUnmount.bind(this);
+  }
 
-    onChange() {
-        this.setState({model: this.props.store.getState()});
-    }
+  onChange() {
+    this.setState({model: this.props.store.getState()});
+  }
 
-    componentDidMount() {
-        this.props.store.addListener(this.onChange);
-    }
+  componentDidMount() {
+    this.props.store.addListener(this.onChange);
+  }
 
-    componentWillUnmount() {
-        this.props.store.removeListener(this.onChange);
-    }
+  componentWillUnmount() {
+    this.props.store.removeListener(this.onChange);
+  }
 
-    render() {
-        return (<div>
-            <span>Hello world!</span>
-            <World model={this.state.model} dispatch={this.props.dispatch}/>
-        </div>);
-    }
+  render() {
+    return (<div>
+      <span>Hello world!</span>
+      <World model={this.state.model} dispatch={this.props.dispatch}/>
+    </div>);
+  }
 }
